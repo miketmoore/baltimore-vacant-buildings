fe95e5e134e203b7e1468dd11b436fa434984119
Michael Moore <miketmoore@gmail.com>
2016-05-17 12:09:34 -0400

    Add blurb to home page

--- src/PageHome.js
+++ src/PageHome.js
@@ -3,6 +3,11 @@ var Layout = require('./Layout');
 
 module.exports = React.createClass({
     render () {
-        return (<Layout>Home Page</Layout>)
+        return (
+            <Layout>
+                <p>This is a single page application that loads a single data source from a server-side JSON file.</p>
+                <p>The app is powered by express.js, React, Bootstrap, paper.js, and various other dependencies.</p>
+            </Layout>
+        )
     }
 })
