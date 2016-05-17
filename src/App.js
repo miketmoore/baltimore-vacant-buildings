bd2768e3285041c58444adf6776da87856eaf800
Michael Moore <miketmoore@gmail.com>
2016-05-17 11:17:05 -0400

    Single page app started, not sharing data yet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {render} from 'react-dom';
 import ReactDataGrid from 'react-data-grid/addons';
 require("react-data-grid/themes/react-data-grid.css");
+var Router = require('react-router-component');
+var Locations = Router.Locations;
+var Location = Router.Location;
 var Model = require('./Model');
 var Nav = require('./Nav');
 var BasicInfo = require('./BasicInfo');
@@ -9,7 +12,7 @@ var PaperBarGraph = require('./Paper/BarGraph');
 
 var dataSource = "data/baltimore-vacant-buildings.json";
 
-var App = React.createClass ({
+var Main = React.createClass ({
     getInitialState () {
         return {
             title: 'Baltimore Vacant Buildings',
@@ -30,6 +33,7 @@ var App = React.createClass ({
     componentWillMount () {
     },
     componentDidMount () {
+        console.log('componentDidMount this.props.source: ', this.props.source);
         this.serverRequest = $.get(this.props.source, function (result) {
             console.log('loaded data source: ', result);
             this.state.model.setRaw(result);
@@ -56,43 +60,64 @@ var App = React.createClass ({
             };
         }
         return (
+            <Layout>
+                <ReactDataGrid
+                    columns={this.state.manualColumns}
+                    rowGetter={rowGetter.bind(this)}
+                    rowsCount={this.state.rows.length}
+                    minHeight={500} />
+            </Layout>
+        );
+    }
+});
+
+var App = React.createClass({
+    render () {
+        return (
+            <Main source={dataSource}/>
+        );
+    }
+});
+
+var Layout = React.createClass({
+    getDefaultProps () {
+        return {
+            title: "Baltimore Vacant Buildings"
+        };
+    },
+    render () {
+        return (
             <div>
-                <Nav title={this.state.title} />
+                <Nav title={this.props.title} />
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12">
-                            <ul id="tabs" className="nav nav-tabs" role="tablist">
-                                <li role="presentation" className="active">
-                                    <a href="#datagrid" aria-controls="datagrid" role="tab" data-toggle="tab">Data Grid</a>
-                                </li>
-                                <li role="presentation">
-                                    <a href="#fields" aria-controls="fields" role="tab" data-toggle="tab">Fields</a>
-                                </li>
-                                <li role="presentation">
-                                    <a href="#bargraph" aria-controls="bargraph" role="tab" data-toggle="tab">Bar Graph</a>
-                                </li>
-                            </ul>
-                            <div className="tab-content">
-                                <div role="tabpanel" className="tab-pane active" id="datagrid">
-                                    <ReactDataGrid
-                                        columns={this.state.manualColumns}
-                                        rowGetter={rowGetter.bind(this)}
-                                        rowsCount={this.state.rows.length}
-                                        minHeight={500} />
-                                </div>
-                                <div role="tabpanel" className="tab-pane" id="fields">
-                                    <BasicInfo model={this.state.model} />
-                                </div>
-                                <div role="tabpanel" className="tab-pane" id="bargraph">
-                                    <PaperBarGraph model={this.state.model} />
-                                </div>
-                              </div>
+                            {this.props.children}
                         </div>
                     </div>
                 </div>
             </div>
         );
     }
+});
+
+var Foo = React.createClass({
+    render () {
+        return (
+            <Layout>
+                Foo
+            </Layout>
+        );
+    }
 })
 
-render(<App source={dataSource} />, document.getElementById('app'));
+{/* render(<App source={dataSource} />, document.getElementById('app')); */}
+render(
+    <Locations hash childProps={{foo: 'bar'}}>
+      <Location path="/" handler={App} />
+      <Location path="/foo" handler={Foo} />
+      <Location path="/basic-info" handler={BasicInfo} />
+      <Location path="/bar-graph" handler={PaperBarGraph} />
+    </Locations>,
+    document.getElementById('app')
+);
--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var Link = require('react-router-component').Link;
 
 module.exports = React.createClass ({
     getDefaultProps () {
@@ -10,7 +11,17 @@ module.exports = React.createClass ({
         return (
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">{this.props.title}</a>
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">{this.props.title}</a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            <li><Link href="/">App</Link></li>
+                            <li><Link href="/foo">Foo</Link></li>
+                            <li><Link href="/basic-info">Basic Info</Link></li>
+                            <li><Link href="/bar-graph">Bar Graph</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         )
