6c11082bd77eaa2ec6e01729a1b304de2064d126
Michael Moore <miketmoore@gmail.com>
2016-05-18 19:41:37 -0400

    Showing 20 markers from data

--- src/MapView.js
+++ src/MapView.js
@@ -4,7 +4,8 @@ import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
 module.exports = React.createClass({
     getInitialState () {
         return {
-            coords: []
+            centerCoords: [],
+            markers: []
         };
     },
     getDefaultProps () {
@@ -14,32 +15,48 @@ module.exports = React.createClass({
     },
     componentWillReceiveProps (props) {
         console.log('MapView.componentWillReceiveProps() props: ', props);
-        // extract coords
+        // extract centerCoords
         var obj = props.model.index.get(':id').values().next().value;
         var loc = obj.location;
-        var coords = [parseFloat(loc[1]), parseFloat(loc[2])];
-        console.log('>>> ', obj, coords);
+        var centerCoords = [parseFloat(loc[1]), parseFloat(loc[2])];
+        console.log('sample obj and centerCoords ', obj, centerCoords);
+
+        var markers = new Set();
+        var count = 0;
+        for ( var obj of props.model.index.get(':id').values() ) {
+            if (count == 50) break;
+            var lat = parseFloat(obj.location[1]);
+            var lng = parseFloat(obj.location[2]);
+            markers.add({
+                position: {
+                    lat: lat,
+                    lng: lng
+                },
+                key: obj[':id'],
+                defaultAnimation: 2
+            });
+            count++;
+        }
+
         this.setState({
-            coords: coords
+            sampleObj: obj,
+            centerCoords: centerCoords,
+            markers: Array.from(markers.values())
         });
 
     },
     render () {
         console.log('Map.render()');
+        if (!this.state.centerCoords.length) return null;
         var props = {
-            markers: {
-                map: function () {
-
-                }
-            },
+            markers: this.state.markers,
             onMapClick: function () {
-
+                console.log('onMapClick(): ', arguments);
             },
-            onMarkerRightclick: function () {
-
+            onMarkerRightclick: function (index) {
+                console.log('onMarkerRightclick() index: ', index);
             }
         };
-        if (!this.state.coords.length) return null;
         return (
 
             <div className="row" style={{height: "100%"}}>
@@ -49,22 +66,21 @@ module.exports = React.createClass({
                             <div
                                 style={{
                                     display: 'block',
-                                    height: '400px',
-                                    width: '400px'
+                                    width: '100%',
+                                    height: '500px'
                                 }}
                             />
                         }
                         googleMapElement={
                             <GoogleMap
-                                ref={(map) => console.log('map: ', map)}
-                                defaultZoom={3}
-                                defaultCenter={{ lat: this.state.coords[0], lng: this.state.coords[1] }}
+                                ref={(map) => console.log('GoogleMap component ref map: ', map)}
+                                defaultZoom={20}
+                                defaultCenter={{ lat: this.state.centerCoords[0], lng: this.state.centerCoords[1] }}
                                 onClick={props.onMapClick}
                             >
                                 {props.markers.map((marker, index) => {
                                     return (
-                                        <Marker
-                                            onRightclick={() => props.onMarkerRightclick(index)} />
+                                        <Marker {...marker} key={index} onRightclick={() => props.onMarkerRightclick(index)} />
                                     );
                                 })}
                             </GoogleMap>
