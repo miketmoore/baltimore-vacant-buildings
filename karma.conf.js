05fab36226ab2df73580a92dccbf3f213436f31a
Michael Moore <miketmoore@gmail.com>
2016-05-21 08:33:05 -0400

    Select.js seems to be loaded w/syntax error

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,54 @@
 var webpack = require('webpack');
+var path = require('path');
+var APP_DIR = path.resolve(__dirname, 'src');
 
-module.exports = function (config) {
-  config.set({
+module.exports = function(config) {
+    config.set({
 
-    browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ],
+        browsers: [process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome'],
 
-    singleRun: true,
+        singleRun: true,
 
-    frameworks: [ 'mocha' ],
+        frameworks: ['mocha'],
 
-    files: [
-      'tests.webpack.js'
-    ],
+        files: [
+            './src/Select.js',
+            'tests.webpack.js'
+        ],
 
-    preprocessors: {
-      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
-    },
+        preprocessors: {
+            'tests.webpack.js': ['webpack', 'sourcemap']
+        },
 
-    reporters: [ 'dots' ],
+        reporters: ['dots'],
 
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        loaders: [
-          { test: /\.js$/, loader: 'babel-loader' }
-        ]
-      }
-    },
+        webpack: {
+            devtool: 'inline-source-map',
+            module: {
+                loaders: [{
+                    test: /\.js$/,
+                    include: APP_DIR,
+                    loader: 'babel',
+                    query: {
+                        presets: ['es2015', 'stage-0', 'react']
+                    }
+                },{
+                    test: /\.css$/, // Only .css files
+                    loader: 'style!css' // Run both loaders
+                }]
+            },
+            resolve: {
+                // tell webpack to look for required files in bower and node
+                modulesDirectories: [
+                    'bower_components',
+                    'node_modules'
+                ],
+            }
+        },
 
-    webpackServer: {
-      noInfo: true
-    }
+        webpackServer: {
+            noInfo: true
+        }
 
-  });
+    });
 };
