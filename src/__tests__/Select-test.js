b5b56d4018cde30e5f9d74355b9524fe58cb4ebb
Michael Moore <miketmoore@gmail.com>
2016-05-21 08:33:05 -0400

    Select.js seems to be loaded w/syntax error

--- src/__tests__/Select-test.js
+++ src/__tests__/Select-test.js
@@ -1,5 +1,5 @@
-describe('Select', function () {
-  it('loads without problems', function () {
-    require('../Select');
-  });
-});
+var React = require('react');
+var TestUtils = require('react/lib/ReactTestUtils'); //I like using the Test Utils, but you can just use the DOM API instead.
+var expect = require('expect');
+
+var Select = require('../Select');
