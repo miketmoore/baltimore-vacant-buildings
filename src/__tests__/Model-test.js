7454a696abd71771f203e94b4d97853ffa3508b6
Michael Moore <miketmoore@gmail.com>
2016-05-21 13:19:45 -0400

    More Model tests

--- src/__tests__/Model-test.js
+++ src/__tests__/Model-test.js
@@ -5,7 +5,80 @@ var should = require('should');
 
 var Model = require('../Model');
 
-describe('Model', function () {
+var sampleDataColDefs = [
+    null,
+    { fieldName: ':id' },
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    { fieldName: 'buildingaddress'},
+    {fieldName: 'noticedate'},
+    {fieldName: 'neighborhood'},
+    {fieldName: 'policedistrict'},
+    {fieldName: 'councildistrict'},
+    {fieldName: 'location'}
+];
+
+var sampleData = [
+    [
+        null,
+        "67F0BF20-05D7-40D4-89AE-4D323757158D",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        "2017 W NORTH AVE",
+        '2016-01-14T00:00:00',
+        "EASTERWOOD",
+        "WESTERN",
+        "7",
+        [
+            null,
+            "39.30943944",
+            "-76.65044524",
+            null,
+            false
+        ]
+    ],
+    [
+        null,
+        "2F623C7A-85E2-42AC-A9CB-C6C241BAD890",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        "1041 N FULTON AVE",
+        "2016-01-08T00:00:00",
+        "SANDTOWN-WINCHESTER",
+        "WESTERN",
+        "9",
+        [
+            null,
+            "39.30077843",
+            "-76.64556524",
+            null,
+            false
+        ]
+    ]
+];
+
+describe('Model ', function () {
     var model;
     beforeEach(function () {
         model = new Model();
@@ -16,7 +89,7 @@ describe('Model', function () {
     it('should have expected public api', function() {
         model.should.have.property('setRaw');
     });
-    describe('setRaw', function () {
+    describe('setRaw rejections', function () {
         it('should reject w/out required args', function (done) {
             model.setRaw().catch(e => done());
         });
@@ -32,20 +105,27 @@ describe('Model', function () {
             model.setRaw({ meta: { view: {} } }).catch(isDone);
             model.setRaw({ meta: { view: { columns: [] } } }).catch(isDone);
         });
+    });
+    describe('setRaw resolves', function () {
+        var valid;
+        beforeEach(() => valid = {
+            meta: {
+                view: {
+                    columns: sampleDataColDefs
+                }
+            },
+            data: sampleData
+        })
         it('should resolve w/valid raw data', function (done) {
-            var valid = {
-                meta: {
-                    view: {
-                        columns: [{
-                            fieldName: ':id'
-                        }]
-                    }
-                },
-                data: [
-
-                ]
-            };
             model.setRaw(valid).then(function () {
+                done();
+            });
+        });
+        it('should have working getters', function () {
+            model.setRaw(valid).then(function () {
+                var rows = model.rows;
+                rows.should.be('array');
+                rows.length.should.equal(sampleData.length);
                 done();
             });
         });
