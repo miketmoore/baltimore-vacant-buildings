a1cbabb9c01ee25d06411cd57f53cc5d3865c153
Michael Moore <miketmoore@gmail.com>
2016-05-22 21:18:57 -0400

    wip

--- src/CustomMap.js
+++ src/CustomMap.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var geolib = require('geolib');
 
 module.exports = React.createClass({
     getDefaultProps () {
@@ -51,7 +52,64 @@ module.exports = React.createClass({
             strokeColor: 'black'
         });
 
+        var ranges = this._determineRange();
+        console.log('ranges: ', ranges);
+        console.log(ranges.latRange);
+        console.log(ranges.lngRange);
+        //console.log(ranges.latRange[0] * 10);
+        this._drawDots(pscope);
+
         pscope.view.update();
+    },
+    _determineRange () {
+        var data = this.props.data.slice(0,5);
+        var entry;
+        var lat, lng, latMin, latMax, lngMin, lngMax;
+        for ( var i = 0, len = data.length; i < len; i++ ) {
+            entry = data[i];
+            lat = entry.latitude;
+            lng = entry.longitude;
+            console.log(lat,lng);
+            if (typeof latMin === 'undefined') latMin = lat;
+            if (typeof latMax === 'undefined') latMax = lat;
+            if (typeof lngMin === 'undefined') lngMin = lng;
+            if (typeof lngMax === 'undefined') lngMax = lng;
+            if (lat < latMin) latMin = lat;
+            if (lat > latMax) latMax = lat;
+            if (lng < lngMin) lngMin = lng;
+            if (lng > lngMax) lngMax = lng;
+        }
+        return {
+            latRange: [latMin,latMax],
+            lngRange: [lngMin,lngMax]
+        };
+    },
+    _drawDots (pscope) {
+        var data = this.props.data.slice(0,10);
+        var obj;
+        var point;
+        var lat;
+        var lng;
+        var latC;
+        var lngC;
+        for ( var i = 0, len = data.length; i < len; i++ ) {
+            obj = data[i];
+            lat = obj.latitude;
+            lng = obj.longitude;
+            console.log(lat, lng);
+            //point = new pscope.Point(Math.ceil(obj.latitude)*.10,-Math.ceil(obj.longitude)*.10);
+            //console.log(lat, lng);
+            point = new pscope.Point({
+                x: lat,
+                y: lng
+            });
+            new pscope.Path.Rectangle({
+                point: point,
+                size: new pscope.Size(4,4),
+                fillColor: 'black',
+                selected: true
+            });
+        }
     },
     componentDidUpdate () {
         this._draw();
--- src/PageCustomMap.js
+++ src/PageCustomMap.js
@@ -8,10 +8,26 @@ module.exports = React.createClass({
             papers: [new paper.PaperScope()]
         };
     },
+    _getMapData (entries) {
+        var data = [];
+        var entries = this.props.model.rows;
+        var entry;
+        for ( var i = 0; i < entries.length; i++ ) {
+            entry = entries[i];
+            data.push({
+                key: entry[':id'],
+                address: entry.buildingaddress,
+                latitude: parseFloat(entry.location[1]),
+                longitude: parseFloat(entry.location[2])
+            });
+        }
+        return data;
+    },
     render () {
+        var data = this._getMapData();
         return (
             <Layout>
-                <CustomMap paper={this.props.papers[0]} />
+                <CustomMap data={data} paper={this.props.papers[0]} />
             </Layout>
         );
     }
