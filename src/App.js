6f7ad84cf0f7c5f1b40c75b6323a1e9c88082254
Michael Moore <miketmoore@gmail.com>
2016-05-30 08:47:17 -0400

    Add missing Model dep to App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import {render} from 'react-dom';
 require("react-data-grid/themes/react-data-grid.css");
 
 var Top = require('./Top');
-
-
-var dataSource = "data/baltimore-vacant-buildings.json";
-
+var Model = require('./Model');
 var model = new Model();
+var dataSource = "data/baltimore-vacant-buildings.json";
 
 render(
     <Top 
